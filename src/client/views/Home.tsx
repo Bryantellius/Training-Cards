cf2f1039e074a2ca666154929119261f95323c98
Ben Bryant <brbryant2639@gmail.com>
2020-05-26 09:17:05 -0500

    Added search input and btn groups to filter column. Started functionality of displaying search results.

--- src/client/views/Home.tsx
+++ src/client/views/Home.tsx
@@ -4,64 +4,91 @@ import { apiService } from "../utils/apiService";
 
 const Home: React.FC<IHomeProps> = () => {
   const [shoes, setShoes] = React.useState<Shoes[]>([]);
-  const [currentShoe, setCurrentShoe] = React.useState<Shoes>(null);
-  const [num, setNum] = React.useState<any>(0);
+  const [query, setQuery] = React.useState<string>("");
+
+  const displayShoes = async () => {
+    let shoes = await apiService("/api/shoes");
+    if (query !== "") {
+      let filteredShoes = shoes.filter(
+        (shoe: Shoes) =>
+          shoe.model_name.indexOf(query) !== -1 ||
+          shoe.brand_name.indexOf(query) !== -1
+      );
+      setShoes(filteredShoes);
+    } else {
+      setShoes(shoes);
+    }
+  };
 
   React.useEffect(() => {
-    (async () => {
-      let shoes = await apiService("/api/shoes");
-      setShoes(shoes);
-      setCurrentShoe(shoes[num]);
-    })();
-  }, []);
+    displayShoes();
+  }, [query]);
 
-  const slide = (index?: any) => {
-    if (index) {
-      setNum(index);
-      setCurrentShoe(shoes[index.toString()]);
-    } else if (num === shoes.length - 1) {
-      setNum(0);
-      setCurrentShoe(shoes[0]);
-    } else {
-      setNum(num + 1);
-      setCurrentShoe(shoes[num + 1]);
-    }
+  const modalView = (shoeIndex?: any) => {
+    // let modal = document.getElementById("modalDiv");
+    // modal.style.display = "block";
+    alert("Display shoe info. TBC.");
   };
 
   return (
     <main className="container my-5">
       <div className="row">
+        {/* Left hand column for search and filter options */}
         <div className="col-sm-4">
-          <ul className="list-group">
-            {shoes.map((shoe, index) => (
-              <li
-                className="list-group-item"
-                key={`${shoe.id}-${shoe.model_name}`}
-                onClick={() => slide(index)}
-              >
-                {shoe.brand_name} {shoe.model_name}
-              </li>
-            ))}
-          </ul>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              placeholder="Search.."
+              value={query}
+              className="form-control"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setQuery(e.target.value)
+              }
+            />
+          </div>
+          <hr />
+          <div className="btn-group mb-3 d-flex" role="group">
+            <button className="btn btn-outline-dark">Neutral</button>
+            <button className="btn btn-outline-dark">Support</button>
+          </div>
+          <div className="btn-group mb-3 d-flex" role="group">
+            <button className="btn btn-outline-dark">Road</button>
+            <button className="btn btn-outline-dark">Trail</button>
+          </div>
+          <div className="btn-group mb-3 d-flex" role="group">
+            <button className="btn btn-outline-dark">Men's</button>
+            <button className="btn btn-outline-dark">Women's</button>
+          </div>
         </div>
         <div className="col-sm-8">
-          <div className="card">
-            <div className="card-img">
-              <img src="" alt="Image of Shoe" />
-            </div>
-            <div className="card-body">
-              <div className="card-text">
-                <h3>
-                  {currentShoe?.brand_name} {currentShoe?.model_name}
-                </h3>
-                <span className="d-block">{currentShoe?.type}</span>
-                <span className="d-block">{currentShoe?.purpose} Running</span>
-                <span className="d-block">${currentShoe?.price}</span>
+          <div className="row row-cols-1 row-cols-md-2">
+            {shoes.map((shoe) => (
+              <div className="col mb-3" key={`${shoe.id}-${shoe.model_name}`}>
+                <div className="card">
+                  <div className="card-img">
+                    <img src="" alt="Image of Shoe" />
+                  </div>
+                  <div className="card-body">
+                    <div className="card-text">
+                      <h5>
+                        {shoe.brand_name} {shoe.model_name}
+                      </h5>
+                      <span className="d-block">{shoe.type}</span>
+                      <span className="d-block">{shoe.purpose} Running</span>
+                      <span className="d-block">${shoe.price}</span>
+                    </div>
+                  </div>
+                  <div className="card-footer">
+                    <button
+                      className="btn btn-primary btn-sm"
+                      onClick={() => modalView()}
+                    >
+                      View
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="card-footer">
-              <button onClick={() => slide()}>Next</button>
-            </div>
+            ))}
           </div>
         </div>
       </div>
